5dc9c62b7260b2c0902227701890ece3f95d9f08
Muhannad Nasri <35005004+Muhannadnasri@users.noreply.github.com>
2020-02-23 00:36:55 +0400

    updates

--- lib/Home/Info/info.dart
+++ lib/Home/Info/info.dart
@@ -1,8 +1,14 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:http/http.dart' as http;
 import 'package:skyline_university/Global/appBar.dart';
-import 'package:skyline_university/Global/rowSection.dart';
+import 'package:skyline_university/Global/global.dart';
+
+import 'infoInside.dart';
 
 void main() => runApp(Info());
 
@@ -13,10 +19,13 @@ class Info extends StatefulWidget {
   }
 }
 
+Map info = {};
+
 class _InfoState extends State<Info> {
   @override
   void initState() {
     super.initState();
+    getInfo();
   }
 
   @override
@@ -26,39 +35,167 @@ class _InfoState extends State<Info> {
       resizeToAvoidBottomPadding: true,
       appBar: appBar(context, 'Skyline Info'),
       body: Container(
-        child: ListView(children: <Widget>[
-          Column(
-            children: <Widget>[
-              SizedBox(
-                height: 15,
-              ),
-              rowSection(context, 'images/admission.png', 'images-white/admission.png', 'SUC at the glance',
-                  '/glance'),
-              rowSection(context, 'images/admission.png', 'images-white/admission.png',
-                  'Vision, Mission, Goals & Objectives', '/goals'),
-              rowSection(context, 'images/admission.png', 'images-white/admission.png', 'Board of Governors',
-                  '/board'),
-              rowSection(context, 'images/admission.png', 'images-white/admission.png', 'Founder Chairman',
-                  '/founder'),
-              rowSection(context, 'images/admission.png', 'images-white/admission.png', 'Executive Council',
-                  '/council'),
-              rowSection(context, 'images/admission.png', 'images-white/admission.png', 'Vice Chancellor', '/dean'),
-              rowSection(
+        child: ListView.builder(
+            itemCount: info.keys.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Column(
+                children: <Widget>[
+                  FittedBox(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => Infoinside(
+                                  infoName:
+                                      info[info.keys.toList()[index].toString()]
+                                          ['name'],
+                                  infoImage:
+                                      info[info.keys.toList()[index].toString()]
+                                          ['image'],
+                                  infoContents:
+                                      info[info.keys.toList()[index].toString()]
+                                          ['contents'],
+                                  infoContent:
+                                      info[info.keys.toList()[index].toString()]
+                                          ['content'],
+                                ),
+                              ),
+                            );
 
+                            // Navigator.pushNamed(context, location);
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                                left: 8.0, top: 8, right: 8.0),
+                            child: Container(
+                              width: 380,
+                              height: 50,
+                              decoration: BoxDecoration(
+                                color: isDark(context)
+                                    ? Color(0xFF1F1F1F)
+                                    : Colors.grey[100],
+                                border: Border.all(
+                                  width: 1.0,
+                                  color: isDark(context)
+                                      ? Colors.white
+                                      : Colors.black,
+                                ),
+                                borderRadius: BorderRadius.all(Radius.circular(
+                                        10.0) //         <--- border radius here
+                                    ),
+                              ),
+                              child: Container(
+                                width: 80,
+                                height: 80,
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: <Widget>[
+                                    SizedBox(
+                                      width: 15,
+                                    ),
+                                    Image.asset(
+                                      isDark(context)
+                                          ? 'images/admission.png'
+                                          : 'images-white/admission.png',
+                                      height: 30,
+                                    ),
+                                    SizedBox(
+                                      width: 20,
+                                    ),
+                                    Text(
+                                      info.keys.toList()[index].toString()
+                                      // info[info.keys.toList()[index].toString()]['name']
+                                      ,
+                                      style: TextStyle(
+                                          color: isDark(context)
+                                              ? Colors.white
+                                              : Colors.black),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                ],
+              );
+            }
 
-                  context, 'images/admission.png', 'images-white/admission.png', 'External Advisory Council', '/council'),
+//           children: <Widget>[
+//           Column(
+//             children: <Widget>[
+//               SizedBox(
+//                 height: 15,
+//               ),
 
-                  
-  rowSection(
-                  context, 'images/admission.png', 'images-white/admission.png', 'Corporate Social Responsibility', '/corporate'),  rowSection(
-                  context, 'images/admission.png', 'images-white/admission.png', 'Academic Affairs Council', '/academic'),
-//  rowSection(
-//                   context, 'images/admission.png', 'images-white/admission.png', 'Committees', '/Calendars'),
-            ],
-          ),
-        ]),
+//               rowSection(context, 'images/admission.png', 'images-white/admission.png', 'SUC at the glance',
+//                   '/glance'),
+//               rowSection(context, 'images/admission.png', 'images-white/admission.png',
+//                   'Vision, Mission, Goals & Objectives', '/goals'),
+//               rowSection(context, 'images/admission.png', 'images-white/admission.png', 'Board of Governors',
+//                   '/board'),
+//               rowSection(context, 'images/admission.png', 'images-white/admission.png', 'Founder Chairman',
+//                   '/founder'),
+//               rowSection(context, 'images/admission.png', 'images-white/admission.png', 'Executive Council',
+//                   '/council'),
+//               rowSection(context, 'images/admission.png', 'images-white/admission.png', 'Vice Chancellor', '/dean'),
+//               rowSection(
+
+//                   context, 'images/admission.png', 'images-white/admission.png', 'External Advisory Council', '/council'),
+
+//   rowSection(
+//                   context, 'images/admission.png', 'images-white/admission.png', 'Corporate Social Responsibility', '/corporate'),  rowSection(
+//                   context, 'images/admission.png', 'images-white/admission.png', 'Academic Affairs Council', '/academic'),
+// //  rowSection(
+// //                   context, 'images/admission.png', 'images-white/admission.png', 'Committees', '/Calendars'),
+//             ],
+            //   ),
+            // ]
+            ),
       ),
     );
   }
 
+  Future getInfo() async {
+    Future.delayed(Duration.zero, () {
+      info = {};
+      showLoading(true, context);
+    });
+    try {
+      http.Response response = await http
+          .post(
+            Uri.encodeFull("http://www.muhannadnasri.com/App/about/data.json"),
+          )
+          .timeout(Duration(seconds: 35));
+
+      if (response.statusCode == 200) {
+        setState(() {
+          info = json.decode(response.body);
+        });
+        showLoading(false, context);
+      }
+    } catch (x) {
+      print(x);
+      if (x.toString().contains("TimeoutException")) {
+        showLoading(false, context);
+
+        showError("Time out from server", FontAwesomeIcons.hourglassHalf,
+            context, getInfo);
+      } else {
+        showLoading(false, context);
+        showError(
+            "Sorry, we can't connect", Icons.perm_scan_wifi, context, getInfo);
+      }
+    }
+  }
 }
---
+++ lib/Home/Info/infoInside.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'package:skyline_university/Global/appBar.dart';
+import 'package:skyline_university/Global/exception.dart';
+import 'package:skyline_university/Global/global.dart';
+import 'package:skyline_university/Global/htmlSecation.dart';
+
+void main() => runApp(Infoinside());
+
+class Infoinside extends StatefulWidget {
+  final String infoName;
+  final String infoImage;
+
+  final List infoContents;
+
+  final String infoContent;
+
+  const Infoinside(
+      {Key key,
+      this.infoName,
+      this.infoImage,
+      this.infoContent,
+      this.infoContents})
+      : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _InfoinsideState();
+  }
+}
+
+class _InfoinsideState extends State<Infoinside> {
+  @override
+  void initState() {
+    super.initState();
+
+    // infoJson = [];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([]);
+    return Scaffold(
+      resizeToAvoidBottomPadding: true,
+      appBar: appBar(context, widget.infoName.toString()),
+      body: ListView(children: <Widget>[
+        Container(
+          child: Image.network(
+            widget.infoImage,
+            fit: BoxFit.contain,
+          ),
+        ),
+        Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(
+                  widget.infoName.toString(),
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: isDark(context) ? Colors.white : Colors.black),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 15.0, top: 5),
+                child: Container(
+                  width: 100,
+                  decoration: UnderlineTabIndicator(
+                      borderSide: BorderSide(
+                          width: 3,
+                          color: isDark(context) ? Colors.white : Colors.black,
+                          style: BorderStyle.solid)),
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+            ],
+          ),
+        ),
+        widget.infoContents == null
+            ? Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(
+                  widget.infoContent.toString() == 'null'
+                      ? ''
+                      : widget.infoContent.toString(),
+                  style: TextStyle(
+                      color: isDark(context) ? Colors.white : Colors.black),
+                ),
+              )
+            : ListView.builder(
+              shrinkWrap: true,
+                physics: ClampingScrollPhysics(),
+                itemCount: widget.infoContents.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text(
+                      widget.infoContents.toString() == 'null'
+                          ? ''
+                          : widget.infoContents[index].toString(),
+                      style: TextStyle(
+                          color: isDark(context) ? Colors.white : Colors.black),
+                    ),
+                  );
+                })
+      ]),
+
+      // htmlSecation(
+      //     context,
+      //     widget.jsonInfo[index]['image'],
+      //     widget.jsonInfo[index]['name'],
+      //     widget.jsonInfo[index]['content']);
+    );
+  }
+}
